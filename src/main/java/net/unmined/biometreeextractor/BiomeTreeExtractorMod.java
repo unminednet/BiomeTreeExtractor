a73654ab91785ae09132aedd64c87276b1687f7e
megazyz <megasys@intro.hu>
2026-04-09 01:14:19 +0200

    Updated to Minecraft 26.2

--- src/main/java/net/unmined/biometreeextractor/BiomeTreeExtractorMod.java
+++ src/main/java/net/unmined/biometreeextractor/BiomeTreeExtractorMod.java
@@ -1,17 +1,18 @@
 package net.unmined.biometreeextractor;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLevelEvents;
 
-import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.core.Holder;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.WorldSavePath;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.source.MultiNoiseBiomeSource;
-import net.minecraft.world.biome.source.util.MultiNoiseUtil;
-import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
+import net.minecraft.world.level.biome.Climate;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
 
+import net.minecraft.world.level.storage.LevelResource;
 import net.unmined.biometreeextractor.mixin.TreeLeafNodeAccessor;
 
 import org.slf4j.Logger;
@@ -31,39 +32,42 @@ public class BiomeTreeExtractorMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        ServerWorldEvents.LOAD.register(this::onWorldLoad);
+        ServerLevelEvents.LOAD.register(this::onWorldLoad);
         LOGGER.info("BiomeTreeExtractor is initialized");
     }
 
-    private void onWorldLoad(MinecraftServer minecraftServer, ServerWorld serverWorld) {
+    private void onWorldLoad(MinecraftServer minecraftServer, ServerLevel serverLevel) {
 
-        var dimensionName = serverWorld.getRegistryKey().getValue().toString();
+        var dimensionName = serverLevel.dimension().identifier().toString();
         LOGGER.info(String.format("Extracting biome search tree for dimension %s", dimensionName));
 
         // Access the world's chunk generator to get the BiomeSource
-        if (!(serverWorld.getChunkManager().getChunkGenerator() instanceof NoiseChunkGenerator chunkGenerator)) {
-            LOGGER.info("Extraction skipped: dimension does not use NoiseChunkGenerator");
+        ChunkGenerator chunkGenerator = serverLevel.getChunkSource().getGenerator();
+        if (!(chunkGenerator instanceof NoiseBasedChunkGenerator noiseBasedChunkGenerator)) {
+            LOGGER.info("Extraction skipped: dimension does not use NoiseBasedChunkGenerator");
             return;
         }
 
         // Check if the biome source is an instance of MultiNoiseBiomeSource
-        if (!(chunkGenerator.getBiomeSource() instanceof MultiNoiseBiomeSource multiNoiseBiomeSource)) {
+        if (!(noiseBasedChunkGenerator.getBiomeSource() instanceof MultiNoiseBiomeSource multiNoiseBiomeSource)) {
             LOGGER.info("Extraction skipped: dimension does not use MultiNoiseBiomeSource");
             return;
         }
 
         try {
 
-            var worldPath = serverWorld.getServer().getSavePath(WorldSavePath.ROOT);
+            var worldPath = serverLevel.getServer().getWorldPath(LevelResource.ROOT);
             var sanitizedDimensionName = dimensionName.replaceAll("[^a-zA-Z0-9._]+", "_");
             var fileName = Paths.get(
                             worldPath.toAbsolutePath().toString(),
                             String.format("biometreeextract_%s.json", sanitizedDimensionName))
                     .toString();
 
-            var rootNode = multiNoiseBiomeSource.biomeEntries.right().isPresent()
-                    ? multiNoiseBiomeSource.biomeEntries.right().get().value().getEntries().tree.firstNode
-                    : multiNoiseBiomeSource.biomeEntries.left().get().tree.firstNode;
+            var parameterList = multiNoiseBiomeSource.parameters.map(
+                    direct -> direct,
+                    preset -> preset.value().parameters()
+            );
+            var rootNode = parameterList.index.root;
 
             try (var writer = new BufferedWriter(new FileWriter(fileName))) {
                 DumpTreeNode(rootNode, writer);
@@ -76,25 +80,25 @@ public class BiomeTreeExtractorMod implements ModInitializer {
         }
     }
 
-    private static void DumpTreeNode(MultiNoiseUtil.SearchTree.TreeNode<RegistryEntry<Biome>> node, BufferedWriter writer) throws IOException {
+    private static void DumpTreeNode(Climate.RTree.Node<Holder<Biome>> node, BufferedWriter writer) throws IOException {
         writer.write("{");
 
-        var parameters = node.parameters;
+        var parameters = node.parameterSpace;
 
         writer.write("\"parameters\":[");
-        writer.write(String.join(",", Arrays.stream(parameters).map((MultiNoiseUtil.ParameterRange p) -> "{\"min\":" + p.min() + ",\"max\":" + p.max() + "}").toList()));
+        writer.write(String.join(",", Arrays.stream(parameters).map((Climate.Parameter p) -> "{\"min\":" + p.min() + ",\"max\":" + p.max() + "}").toList()));
         writer.write("],");
 
-        if (node instanceof net.minecraft.world.biome.source.util.MultiNoiseUtil.SearchTree.TreeBranchNode<RegistryEntry<Biome>> n) {
+        if (node instanceof Climate.RTree.SubTree<Holder<Biome>> n) {
             writer.write("\"subTree\":[");
-            var subTree = n.subTree;
+            var subTree = n.children;
             for (var i = 0; i < subTree.length; i++) {
                 DumpTreeNode(subTree[i], writer);
                 if (i < subTree.length - 1) writer.write(",");
             }
             writer.write("]");
-        } else if (node instanceof net.minecraft.world.biome.source.util.MultiNoiseUtil.SearchTree.TreeLeafNode<RegistryEntry<Biome>> l) {
-            var biome = ((RegistryEntry.Reference<Biome>) ((TreeLeafNodeAccessor) (Object) l).getValue()).getKey().get().getValue().toString();
+        } else if (node.getClass().getName().contains("Leaf")) {
+            var biome = ((Holder.Reference<Biome>) ((TreeLeafNodeAccessor) (Object) node).getValue()).key().identifier().toString();
             writer.write("\"biome\":\"" + biome + "\"");
         }
 
--- src/main/java/net/unmined/biometreeextractor/mixin/TreeLeafNodeAccessor.java
+++ src/main/java/net/unmined/biometreeextractor/mixin/TreeLeafNodeAccessor.java
@@ -3,7 +3,7 @@ package net.unmined.biometreeextractor.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(net.minecraft.world.biome.source.util.MultiNoiseUtil.SearchTree.TreeLeafNode.class)
+@Mixin(net.minecraft.world.level.biome.Climate.RTree.Leaf.class)
 public interface TreeLeafNodeAccessor {
     @Accessor
     Object getValue();
